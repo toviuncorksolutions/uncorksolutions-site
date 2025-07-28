0acf407fdc7ca7318aede6476e92cb40575ad4ca
toviuncorksolutions <tovi.heilbronn@gmail.com>
2025-07-28 01:33:27 -0400

    GTM JS changes

--- pages/_app.js
+++ pages/_app.js
@@ -16,19 +16,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       {/* Google Tag Manager (GTM) script */}
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${GTM_ID}');
-          `,
-        }}
-      />
+      <Script src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`} strategy="lazyOnload" />
       <div className={wixFont.className}>
         <Component {...pageProps} />
         <SpeedInsights />
--- pages/_document.js
+++ pages/_document.js
@@ -14,14 +14,14 @@ export default function Document() {
       </Head>
       <body>
         {/* Google Tag Manager (noscript) */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
-        />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
