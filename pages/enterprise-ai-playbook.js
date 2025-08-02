29433e6b2f5d1507dc03ee0d2af21de93dbc3114
toviuncorksolutions <tovi.heilbronn@gmail.com>
2025-08-02 11:57:58 -0400

    Update enterprise-ai-playbook.js

--- pages/enterprise-ai-playbook.js
+++ pages/enterprise-ai-playbook.js
@@ -205,7 +205,7 @@ export default function EnterpriseAIPlaybook() {
         <meta property="og:url" content="https://www.uncorksolutions.com/enterprise-ai-playbook" />
         <meta
           property="og:image"
-          content="https://www.uncorksolutions.com/ai-readiness-playbook-1b.png"
+          content="https://www.uncorksolutions.com/ai-readiness-playbook-1c.png"
         />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
@@ -224,7 +224,7 @@ export default function EnterpriseAIPlaybook() {
         />
         <meta
           name="twitter:image"
-          content="https://www.uncorksolutions.com/ai-readiness-playbook-1b.png"
+          content="https://www.uncorksolutions.com/ai-readiness-playbook-1c.png"
         />
 
         <script
@@ -302,7 +302,7 @@ export default function EnterpriseAIPlaybook() {
                     isPartOf: { '@id': 'https://www.uncorksolutions.com#organization' },
                     primaryImageOfPage: {
                       '@type': 'ImageObject',
-                      url: 'https://www.uncorksolutions.com/ai-readiness-playbook-1b.png',
+                      url: 'https://www.uncorksolutions.com/ai-readiness-playbook-1c.png',
                       width: 760,
                       height: 846,
                     },
@@ -466,7 +466,7 @@ export default function EnterpriseAIPlaybook() {
           >
             <Image
               id="hero-ai-booklet-img"
-              src="ai-readiness-playbook-1b.png"
+              src="ai-readiness-playbook-1c.png"
               alt="Front cover of Uncork Solutions' AI Readiness Assessment sample booklet."
               width={760}
               height={846}
